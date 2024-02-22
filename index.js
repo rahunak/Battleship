13668f8fbaea7e0aa201a0979890e1965812ed6e
rahunak <euhene@linnovate.net>
2024-02-22 22:49:46 +0300

    feat: added simple connection to WebSocketServer

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-import { httpServer } from "./src/http_server/index.js";
+import { httpServer } from './src/http_server/index.js';
+import * as dotenv from 'dotenv';
 
-const HTTP_PORT = 8181;
+dotenv.config();
+let HTTP_PORT = process.env.HTTP_PORT ?? 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+httpServer.on('connection', (socket) => {
+  console.log('httpServer connection');
+  socket.unref();
+});
--- src/http_server/index.js
+++ src/http_server/index.js
@@ -1,17 +1,44 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as http from 'http';
+import { WebSocketServer } from 'ws';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+
+let HTTP_PORT = process.env.HTTP_PORT ?? 8181;
+let WS_PORT = process.env.WS_PORT ?? 3000;
 
 export const httpServer = http.createServer(function (req, res) {
-    const __dirname = path.resolve(path.dirname(''));
-    const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
-    fs.readFile(file_path, function (err, data) {
-        if (err) {
-            res.writeHead(404);
-            res.end(JSON.stringify(err));
-            return;
-        }
-        res.writeHead(200);
-        res.end(data);
-    });
+
+  const __dirname = path.resolve(path.dirname(''));
+  const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
+  fs.readFile(file_path, function (err, data) {
+    if (err) {
+      console.log('readFile err', err);
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
 });
+
+const wss = new WebSocketServer({ port: WS_PORT });
+
+wss.on('connection', function (wsSoket) {
+  console.log('new connection',wsSoket);
+
+  wsSoket.on('message',(msg)=>{
+    console.log('message',JSON.parse(msg,null,4));
+  });
+
+  wsSoket.on('close',()=>{
+    console.log('close');
+  });
+
+
+});
+
